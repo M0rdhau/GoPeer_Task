2087d98bc8b32e26fbcd42356d1be0177574dbae
Dachi Mshvidobadze <dachimshvidobadze@gmail.com>
2021-07-04 23:20:32 +0300

    Backend is mostly done, need to write some tests now

--- back/app.js
+++ back/app.js
@@ -2,8 +2,9 @@ const config = require('./utils/config')
 const express = require('express')
 require('express-async-errors')
 const app = express()
-// const productsRouter = require('./controllers/products');
-// const categoriesRouter = require('./controllers/categories');
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const linkRouter = require('./controllers/links')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
@@ -27,8 +28,9 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-// app.use('/api/products', productsRouter);
-// app.use('/api/categories', categoriesRouter);
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+app.use('/links', linkRouter)
 // app.use('/api-documentation', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use(middleware.unknownEndpoint)
---
+++ back/controllers/users.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+const MIN_LENGTH = 3
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User
+    .find({}).populate('links', { title: 1, author: 1 })
+  response.json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  if(body.username.length < MIN_LENGTH || body.password.length < MIN_LENGTH){
+    return response.status(401).json({ error : 'username or password too short' })
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+  const user = new User({
+    username: body.username,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.json(savedUser)
+})
+
+module.exports = usersRouter
